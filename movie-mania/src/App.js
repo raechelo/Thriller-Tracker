8d2598451e52435b82ddbc65296b6fad708e6341
RyanDBarnett <conner2468@gmail.com>
2019-05-07 14:57:46 -0600

    Fetch movie data from movieDB

--- movie-mania/src/App.js
+++ movie-mania/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { fetchData } from './utils/fetchData';
+import { key } from './utils/apikey';
+// import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>MOVIE MANIA</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount = () => {
+    this.fetchMovies()
+  }
+
+  fetchMovies = async () => {
+    let url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&page=${1}`;
+    try {
+      let allMovies = await fetchData(url);
+      console.log(allMovies);
+    } catch {
+
+    }
+  }
+
+  render() {
+    console.log(key);
+    return (
+      <div className="App">
+        <h1>MOVIE MANIA</h1>
+      </div>
+    );
+  }
 }
+
+// export const mapDispatchToProps = (dispatch) => ({})
+// export const mapStateToProps = (state) => ({})
 
 export default App;
---
+++ movie-mania/src/utils/apikey.js
@@ -0,0 +1,1 @@
+export const key = '7b44bb1f4812a66d32d5ee714c8361a3';
