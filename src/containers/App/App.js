c89dbf2dfbc3c18e087a6baa80bbab6f903e50af
RyanDBarnett <conner2468@gmail.com>
2019-05-07 22:50:08 -0600

    Import fetchMovies and remove unused imports to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import { fetchData } from '../../api/fetchData';
-import { key } from '../../utils/apikey';
+import fetchMovies from '../../api/fetchMovies';
 // import PropTypes from 'prop-types';
 
 class App extends Component {
   componentDidMount = () => {
-    this.fetchMovies()
-  }
-
-  fetchMovies = async () => {
-    let url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&page=${1}`;
-    try {
-      let allMovies = await fetchData(url);
-      console.log(allMovies);
-    } catch {
-
-    }
+    fetchMovies()
   }
 
   render() {
-    console.log(key);
     return (
       <div className="App">
         <h1>MOVIE MANIA</h1>
