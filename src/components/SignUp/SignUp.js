6a773c530f0ce6a8d7879ba0c5460976d7641cb5
RyanDBarnett <conner2468@gmail.com>
2019-05-09 10:50:51 -0600

    Create html for signup form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignUp = () => {
-  return (
-    <h2>SignUp</h2>
-  )
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  // onSubmit={ () => handleSubmit() }
+  render() {
+    return (
+      <form>
+        <h2>Create Account</h2>
+        <label for="name">Name</label>
+        <input onChange={ this.handleChange } name='name' type='text' id="name" />
+        <label for="email">Email</label>
+        <input onChange={ this.handleChange } name='email' type='email' id="email" />
+        <label for="password">Password</label>
+        <input onChange={ this.handleChange } name='password' type='password' id="password" />
+        <button>Create</button>
+      </form>
+    ) 
+  }
 }
 
 export default SignUp;
