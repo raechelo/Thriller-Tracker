6810f382094f2a25f40f41bb23352e77ec6ee816
raechel <odom.raechel@gmail.com>
2019-05-09 20:57:11 -0600

    api call updated to reflect horror movies

--- src/api/fetchMovies.js
+++ src/api/fetchMovies.js
@@ -3,7 +3,7 @@ import fetchData from './fetchData';
 import cleanMovies from './cleanMovies';
 
 const fetchMovies = async () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=14`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=27`
   // const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&page=${1}`;
   const parsedResponse = await fetchData(url);
   return cleanMovies(parsedResponse.results);
