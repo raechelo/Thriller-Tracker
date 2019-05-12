0bd41315e62cccf3a08be45ee832f4b9794bf16d
RyanDBarnett <conner2468@gmail.com>
2019-05-12 12:16:08 -0600

    Create hasErrored and isLoading reducers for movies

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -1,4 +1,4 @@
-export const moviesReducer = (state = [], action) => {
+export const movies = (state = [], action) => {
   switch(action.type) {
     case 'SET_MOVIES':
       return action.movies;
@@ -6,6 +6,24 @@ export const moviesReducer = (state = [], action) => {
       return state.map(movie => {
         return movie.id === action.id ? {...movie, favorited: !movie.favorited} : movie;
       })
+    default:
+     return state;
+  }
+}
+
+export const isLoading = (state = false, action) => {
+  switch(action.type) {
+    case 'IS_LOADING':
+      return action.isLoading;
+    default:
+     return state;
+  }
+}
+
+export const hasErrored = (state = '', action) => {
+  switch(action.type) {
+    case 'HAS_ERRORED':
+      return action.message;
     default:
      return state;
   }
