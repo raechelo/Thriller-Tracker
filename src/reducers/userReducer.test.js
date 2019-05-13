3dc9d3f180614650f3686f561bcd8c03654cdcfe
raechel <odom.raechel@gmail.com>
2019-05-13 15:46:51 -0600

    tests updated and fixed after adding thunks

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -1,24 +1,24 @@
-import { userReducer } from './userReducer';
+import { user } from './userReducer';
 import * as actions from '../actions';
 
-describe('userReducer', () => {
+describe('user', () => {
 
   it('should return default state', () => {
     const expected = {};
 
-    const result = userReducer(undefined, {});
+    const result = user(undefined, {});
     expect(result).toEqual(expected);
   });
 
   it('should return no user upon user logout', () => {
-    const result = userReducer(undefined, actions.logOutUser());
+    const result = user(undefined, actions.logOutUser());
     expect(result).toEqual({})
   });
 
   it('should return a user upon uesr login', () => {
-    const user = { name: 'Heracles', email: 'twelvetimeslaborer@olympus.com', password: 'goldenfleece' }
+    const expected = { name: 'Heracles', email: 'twelvetimeslaborer@olympus.com', password: 'goldenfleece' }
 
-    const result = userReducer(undefined, actions.logInUser(user));
-    expect(result).toEqual(user)
+    const result = user(undefined, actions.logInUser(expected));
+    expect(result).toEqual(expected)
   });
 });
