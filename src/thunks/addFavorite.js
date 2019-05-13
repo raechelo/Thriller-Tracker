cf5b3ede6e71a933bbcd5b83ce971a97207577a0
RyanDBarnett <conner2468@gmail.com>
2019-05-13 17:18:25 -0600

    Create add and delete favorite thunks

---
+++ src/thunks/addFavorite.js
@@ -0,0 +1,24 @@
+import { fetchData } from '../api/fetchData';
+
+export const addFavorite = (movie, user_id) => {
+  return async (dispatch) => {
+    const {id, title, posterImage, release_date, vote_average, overview} = movie;
+    const url = 'http://localhost:3000/api/users/favorites/new'
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        movie_id: id,
+        user_id,
+        title,
+        poster_path: posterImage, 
+        release_date,
+        vote_average,
+        overview
+      })
+    }
+    await fetchData(url, options);
+  }
+}
