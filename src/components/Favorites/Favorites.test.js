4a7e4d1d2ba471f3d8d3e0f51433fc04bdbe3bfc
raechel <odom.raechel@gmail.com>
2019-05-13 22:33:36 -0600

    Tests updated to reflect conditional rendering(favorites) as well as other new additions

--- src/components/Favorites/Favorites.test.js
+++ src/components/Favorites/Favorites.test.js
@@ -5,12 +5,13 @@ import { Favorites } from './Favorites';
 describe('Favorites', () => {
 
   let wrapper;
+  let favorited = [{title: 'Isle of Dogs', favorite: true}]
 
   beforeEach(() => {
-    wrapper = shallow( <Favorites /> )
+    wrapper = shallow( <Favorites movies={favorited} /> )
   });
 
   it('should match the snapshot with all the data passed in', () => {
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot();
   })
 })
