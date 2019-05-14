4d6730b6afaf59b5a1ea78684c19417983266917
RyanDBarnett <conner2468@gmail.com>
2019-05-13 21:04:37 -0600

    Import save/loadState functions to index.js and implement

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './scss/index.scss';
-import App from './containers/App/App';
-import { rootReducer } from './reducers';
 import { Provider } from 'react-redux'; 
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers';
+import { saveState, loadState } from './utils/localStorage';
+import './scss/index.scss';
+import App from './containers/App/App';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const persistedState = loadState();
+const store = createStore(
+  rootReducer,
+  persistedState, 
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 const router = (
   <Provider store={store} >
     <BrowserRouter> 
