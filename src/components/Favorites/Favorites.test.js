3ba621ef40fdb9a30a864953faf5db66285d3e21
RyanDBarnett <conner2468@gmail.com>
2019-05-13 22:40:42 -0600

    Fix favorites test

--- src/components/Favorites/Favorites.test.js
+++ src/components/Favorites/Favorites.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Favorites } from './Favorites';
 
+const mockMovies = [
+  {title: 'Forest Gump'},
+  {title: 'Star Wars'}
+]
+
 describe('Favorites', () => {
 
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow( <Favorites /> )
+    wrapper = shallow( 
+      <Favorites 
+        movies={mockMovies} 
+      /> 
+    )
   });
 
   it('should match the snapshot with all the data passed in', () => {
