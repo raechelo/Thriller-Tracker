96142287fbf0380f0daa507033d7bf41653d4d46
raechel <odom.raechel@gmail.com>
2019-05-14 19:46:44 -0600

    finishing conditional renders for snapshots on containers

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Nav } from './Nav';
+import { getMaxListeners } from 'cluster';
 
 describe('Nav', () => {
 
@@ -13,7 +14,11 @@ describe('Nav', () => {
   });
 
   it('should match the snapshot with all the data passed in', () => {
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot();
+
+    user.email = 'godofolympus@gmail.com';
+
+    expect(wrapper).toMatchSnapshot();
   });
   
 });
---
+++ src/containers/CardDetails/CardDetails.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CardDetails from './CardDetails';
+
+describe('CardDetails', () => {
+  
+  let wrapper;
+  
+  let movie = { title: 'Return of the King', vote_average: 8.9, release_date: "2003-05-10", overview: 'The rightful king retakes the throne of Men and Gondor.'}
+
+  beforeEach(() => {
+    wrapper = shallow(<CardDetails m={movie} />)
+  });
+
+  it('should match the snapshot with all the data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+})
