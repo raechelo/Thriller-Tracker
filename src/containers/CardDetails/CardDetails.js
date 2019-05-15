3f4e6c6e2fcded7b14974bb24d4538167c0f3398
RyanDBarnett <conner2468@gmail.com>
2019-05-14 20:41:15 -0600

    Connect cardDetails and allow users to favorite/unfavorite movie

--- src/containers/CardDetails/CardDetails.js
+++ src/containers/CardDetails/CardDetails.js
@@ -1,41 +1,68 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { toggleFavoriteMovie } from '../../actions';
+import { deleteFavorite } from '../../thunks/deleteFavorite';
+import { addFavorite } from '../../thunks/addFavorite';
 
 class CardDetails extends Component {
-  constructor() {
-    super()
-    this.state = {
-      redirected: false
+
+  handleFavoriteClick = () => {
+    const user_id = this.props.user.id
+    if (!user_id) {
+      return alert('Login or Signup to favorite movies')
+    }
+    const { id, favorited } = this.props;
+    const movie = this.props;
+    
+    this.props.toggleFavoriteMovie(id);
+    if (favorited) {
+      this.props.deleteFavorite(id, user_id);
+    } else {
+      this.props.addFavorite(movie, user_id);
     }
   }
 
-  handleClick = () => {
-    this.setState({ redirected: !this.state.redirected})
-  }
-
-
-    render () {
-      const {title, vote_average, overview, posterImage, release_date, favorited} = this.props
-      const heartClasses = favorited ? 'fas fa-heart fa-2x favorited' : 'fas fa-heart fa-2x';
-
-      return (
-        <section className="Card-Details" >
-          <h2>{title}</h2>
-          <i onClick={this.handleFavoriteClick} className={heartClasses}></i>
-          <h5>Released<span>{release_date.slice(0, 4)}</span></h5>
-          <h6>{vote_average}/10<i className="far fa-star"></i></h6>
-          <p>{overview}</p>
-          <img src={posterImage} alt={title + ' poster'} />
-          <button onClick={this.handleClick} className="back-btn"><i className="fas fa-angle-left"></i> Back to results</button>
-          {this.state.redirected && <Redirect to='/' />}
-        </section>
-      )
+  render () {
+    const {title, vote_average, overview, posterImage, release_date, favorited} = this.props
+    const heartClasses = favorited ? 'fas fa-heart fa-2x favorited' : 'fas fa-heart fa-2x';
+    return (
+      <section className="Card-Details" >
+        <h2>{title}</h2>
+        <i onClick={this.handleFavoriteClick} className={heartClasses}></i>
+        <h5>Released<span>{release_date.slice(0, 4)}</span></h5>
+        <h6>{vote_average}/10<i className="far fa-star"></i></h6>
+        <p>{overview}</p>
+        <img src={posterImage} alt={title + ' poster'} />
+        <Link to='/'>
+          <button className="back-btn">
+            <i className="fas fa-angle-left"></i> Back to results
+          </button>
+        </Link>
+      </section>
+    )
   }
 }
 
 CardDetails.propTypes = {
-  m: PropTypes.object
+  id: PropTypes.number,
+  title: PropTypes.string, 
+  vote_average: PropTypes.number,
+  overview: PropTypes.string, 
+  posterImage: PropTypes.string, 
+  release_date: PropTypes.string, 
+  favorited: PropTypes.bool
 }
 
-export default CardDetails;
+export const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  toggleFavoriteMovie: (id) => dispatch(toggleFavoriteMovie(id)),
+  addFavorite: (movie, user_id) => dispatch(addFavorite(movie, user_id)),
+  deleteFavorite: (movie_id, user_id) => dispatch(deleteFavorite(movie_id, user_id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardDetails);
