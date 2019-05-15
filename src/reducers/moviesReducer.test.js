5d819c324f449995d2675788c5013d1ffd695c8c
raechel <odom.raechel@gmail.com>
2019-05-14 22:07:25 -0600

    testing added for additional reducers

--- src/reducers/moviesReducer.test.js
+++ src/reducers/moviesReducer.test.js
@@ -1,4 +1,6 @@
 import { movies } from './moviesReducer';
+import { isLoading } from './moviesReducer';
+import { hasErrored } from './moviesReducer';
 import * as actions from '../actions';
 
 describe('movies', () => {
@@ -22,6 +24,40 @@ describe('movies', () => {
     const expected = [ { id, favorited: false } ];
     
     const result = movies([ { id, favorited: true } ], actions.toggleFavoriteMovie(id));
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('isLoading', () => {
+
+  it('should return the initial state', () => {
+    const expected = false;
+    const result = isLoading(undefined, false)
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a truthy value in case of loading', () => {
+    const expected = true;
+    const result = isLoading(undefined, actions.isLoading(true));
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('hasErrored', () => {
+
+  it('shoud return a default state', () => {
+    const expected = '';
+    const result = hasErrored(undefined, '');
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an error message', () => {
+    const expected = 'Error!';
+    const result = hasErrored(undefined, actions.hasErrored('Error!'))
+
     expect(result).toEqual(expected);
   });
 });
