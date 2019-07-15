9e95db2a386cd6bef506601d9bda9d325b974f49
raechel <odom.raechel@gmail.com>
2019-07-15 10:16:25 -0600

    Updates

---
+++ precache-manifest.2a72f730a227ad3e10b16f3a1f2ba143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c703804a7ea3570794e310edfd725e1e",
+    "url": "/thriller-tracker/index.html"
+  },
+  {
+    "revision": "1422995f1c2a93c6f236",
+    "url": "/thriller-tracker/static/css/main.5123f661.chunk.css"
+  },
+  {
+    "revision": "2e8480c9f4702810b19e",
+    "url": "/thriller-tracker/static/js/2.615bc975.chunk.js"
+  },
+  {
+    "revision": "1422995f1c2a93c6f236",
+    "url": "/thriller-tracker/static/js/main.6f365df1.chunk.js"
+  },
+  {
+    "revision": "f8bc40b4b70eea6caa45",
+    "url": "/thriller-tracker/static/js/runtime~main.42f24c5b.js"
+  }
+]);
